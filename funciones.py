66d94a76ca532597e92e282dd8d7ff71f99937cc
JaimeCastilla <jaicascob12@iesfidiana.es>
2026-05-14 10:26:09 +0200

    Ordenar practicas_python numerando practicas del 01 al 20

---
+++ funciones.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+nombre=input("¿Cuál es tu nombre?: ")
+print(f"Hola {nombre}, bienvenido a la programación en Python")
+
+carpeta_personal = Path.home()
+
+print(f"Tu carpeta personal es: {nombre}")
+    
