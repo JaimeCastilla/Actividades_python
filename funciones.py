19f54e11b767349a8b0097c3f1921879af89a33e
JaimeCastilla <jaicascob12@iesfidiana.es>
2026-05-14 10:23:40 +0200

    Eliminar funciones.py del repositorio

--- funciones.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-nombre=input("¿Cuál es tu nombre?: ")
-print(f"Hola {nombre}, bienvenido a la programación en Python")
-
-carpeta_personal = Path.home()
-
-print(f"Tu carpeta personal es: {nombre}")
-    
